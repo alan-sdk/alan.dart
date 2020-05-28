e971e3e91116d481b923a9bbee8340c916f63b61
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-05-28 08:45:41 +0200

    Updated the documentation of CosmosAccount as well as added a copyWith method

--- lib/models/cosmos-sdk/query/account.dart
+++ lib/models/cosmos-sdk/query/account.dart
@@ -10,16 +10,24 @@ part 'account.g.dart';
 @immutable
 @JsonSerializable(explicitToJson: true)
 class CosmosAccount extends Equatable {
-  @JsonKey(name: "address")
+  /// Address associated to the the account, is Bech32 format.
+  /// If the user does not exist on-chain, this will be an empty string.
+  @JsonKey(name: "address", nullable: false, defaultValue: "")
   final String address;
 
-  @JsonKey(name: "account_number")
+  /// Account number on-chain.
+  /// If the account does not exist on-chain this will be -1.
+  @JsonKey(name: "account_number", nullable: false, defaultValue: -1)
   final int accountNumber;
 
-  @JsonKey(name: "sequence")
+  /// Account sequence on-chain.
+  /// If the account does not exist on-chain this will be -1.
+  @JsonKey(name: "sequence", nullable: false, defaultValue: -1)
   final int sequence;
 
-  @JsonKey(name: "coins")
+  /// Coins associated to this account.
+  /// If the account does not exist on-chain, this will be an empty array.
+  @JsonKey(name: "coins", nullable: false, defaultValue: [])
   final List<StdCoin> coins;
 
   const CosmosAccount({
@@ -32,21 +40,46 @@ class CosmosAccount extends Equatable {
         assert(sequence != null),
         assert(coins != null);
 
+  /// Allows to easily create a local account having everything set as
+  /// the default not on-chain values a part from the given [address].
   factory CosmosAccount.offline(String address) {
     return CosmosAccount(
       address: address,
-      accountNumber: 0,
-      sequence: 0,
+      accountNumber: -1,
+      sequence: -1,
       coins: [],
     );
   }
 
+  /// Builds a [CosmosAccount] instance by parsing the given [json] map.
   factory CosmosAccount.fromJson(Map<String, dynamic> json) {
     return _$CosmosAccountFromJson(json);
   }
 
+  /// Transforms this instance of [CosmosAccount] into a JSON map.
   Map<String, dynamic> toJson() {
     return _$CosmosAccountToJson(this);
+  }
+
+  /// Allows to create a copy instance of this [CosmosAccount] containing the
+  /// same exact data, with the ones given replaced.
+  CosmosAccount copyWith({
+    String address,
+    int accountNumber,
+    int sequence,
+    List<StdCoin> coins,
+  }) {
+    return CosmosAccount(
+      address: address ?? this.address,
+      accountNumber: accountNumber ?? this.accountNumber,
+      sequence: sequence ?? this.sequence,
+      coins: coins ?? this.coins,
+    );
+  }
+
+  @override
+  List<Object> get props {
+    return [address, accountNumber, sequence, coins];
   }
 
   @override
@@ -57,10 +90,5 @@ class CosmosAccount extends Equatable {
         'sequence: $sequence, '
         'coins: $coins '
         '}';
-  }
-
-  @override
-  List<Object> get props {
-    return [address, accountNumber, sequence, coins];
   }
 }
